bfb252b4cef614957eaecbd50940d8c0098e8931
yanghl <yanghl@ngarihealth.com>
2018-01-26 17:55:53 +0800

    SpringCloud服务治理

---
+++ eureka-client/src/main/java/com/jeecx/springcloud/EurekaClientApplication.java
@@ -0,0 +1,32 @@
+package com.jeecx.springcloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @EnableDiscoveryClient注解，该注解能激活Eureka中的DiscoveryClient实现
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class EurekaClientApplication {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping("/dc")
+	public String dc() {
+		String services = "Services: " + discoveryClient.getServices();
+		System.out.println(services);
+		return services;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
